dc2b784a58a706c76fd44177f56617b65a61dc64
aclyde11 <aclyde11@gmail.com>
2016-08-29 20:35:45 -0400

    added hashing

---
+++ src/Hashing/MyEntry.java
@@ -0,0 +1,14 @@
+package Hashing;
+
+/**
+ * Created by austin on 8/29/16.
+ */
+public class MyEntry<K, V>{
+    K key;
+    V value;
+
+    public MyEntry (K k, V v){
+        key = k;
+        value = v;
+    }
+}
---
+++ src/MyArrayList.java
@@ -0,0 +1,9 @@
+/**
+ * Created by austin on 8/29/16.
+ * For academic use
+ */
+public class MyArrayList<T> {
+
+    private Object[] list;
+
+}
